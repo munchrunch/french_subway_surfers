a8d11d71dc99b263876d06c42b4c9c2b553926a4
munchrunch <muntra.dadjoo@gmail.com>
2025-07-08 10:51:35 -0400

    preliminary logic

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 package french_subway_surfers;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JPanel {
     public static void main (String [] args){
         System.out.println("test");
     }
+
+    /* program notes/preliminary logic:
+     *  - need to make it so that character automatically keeps going forward
+     *  - 2d game, character can only go up and down (maybe a bird??)
+     *  - add obstacles later, just make sure that character keeps going forward
+     *  - need to design character and background (something pixelated, y2k style)
+     *  - player object, make screen little smaller than laptop monitor size, etc.
+     */
 }
